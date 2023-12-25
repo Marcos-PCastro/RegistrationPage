a8aef99bdcbdb9e0c547b6eb6ba4b3661ffbe969
Marcos-PCastro <courrielprive@yahoo.com.br>
2023-12-25 14:14:07 -0300

    Setting up the first page model

---
+++ RegistrationPage/Models/Registro.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistrationPage.Models
+{
+    public class Registro
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Título")]
+        [Required(ErrorMessage = "Campo Obrigatorio")]
+        public string Title { get; set; }
+
+        [DisplayName ("Concluído")]
+        public bool Done { get; set; }
+
+        [DisplayName ("Criado em")]
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        [DisplayName ("Ultima Atualização")]
+        public DateTime LastUpdateDate { get; set; } = DateTime.Now;
+
+        [DisplayName ("Usúario")]
+        public string User { get; set; }
+    }
+}
